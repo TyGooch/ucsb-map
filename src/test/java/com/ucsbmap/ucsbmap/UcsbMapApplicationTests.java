14777738c55e615b6c79b7b923b7f5c94968df60
Ty Gooch <gooch.ty@gmail.com>
2018-02-08 01:03:49 -0800

    Spring Boot and React boilerplate

---
+++ src/test/java/com/ucsbmap/ucsbmap/UcsbMapApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucsbmap.ucsbmap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UcsbMapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
