efc77ee4e4fc948f0bf3f18e261b768cf28d1f4f
Ty Gooch <gooch.ty@gmail.com>
2018-02-20 05:22:59 -0800

    refactored controller

--- src/main/java/com/ucsbmap/controllers/LocationController.java
+++ src/main/java/com/ucsbmap/controllers/LocationController.java
@@ -28,31 +28,4 @@ public class LocationController {
         return locationRepository.save(location);
     }
 
-    // @GetMapping(value="/locations/{id}")
-    // public ResponseEntity<Location> getLocationById(@PathVariable("id") String id) {
-    //     Location location = locationRepository.findOne(id);
-    //     if(location == null) {
-    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    //     } else {
-    //         return new ResponseEntity<>(location, HttpStatus.OK);
-    //     }
-    // }
-    // 
-    // @PutMapping(value="/locations/{id}")
-    // public ResponseEntity<Location> updateLocation(@PathVariable("id") String id,
-    //                                        @Valid @RequestBody Location location) {
-    //     Location locationData = locationRepository.findOne(id);
-    //     if(locationData == null) {
-    //         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    //     }
-    //     locationData.setTitle(location.getTitle());
-    //     locationData.setCompleted(location.getCompleted());
-    //     Location updatedLocation = locationRepository.save(locationData);
-    //     return new ResponseEntity<>(updatedLocation, HttpStatus.OK);
-    // }
-    // 
-    // @DeleteMapping(value="/locations/{id}")
-    // public void deleteLocation(@PathVariable("id") String id) {
-    //     locationRepository.delete(id);
-    // }
 }
