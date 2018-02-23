9ba198e527af16c981c88bf6d3b78de8d84525d8
Ty Gooch <gooch.ty@gmail.com>
2018-02-23 10:13:34 -0800

    fixed url for AJAX request so mobile will work

--- src/main/app/src/util/locationAPIUtil.js
+++ src/main/app/src/util/locationAPIUtil.js
@@ -1,5 +1,5 @@
 export const fetchLocations = () => (
-  fetch('http://localhost:8080/api/locations')
+  fetch('/api/locations')
     .then(response => {
       if (!response.ok) {
         throw new Error(`status ${response.status}`)
