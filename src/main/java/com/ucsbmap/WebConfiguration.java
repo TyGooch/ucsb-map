b4ad8b155288f86ea9b5eefa33467e0ff3834838
Ty Gooch <gooch.ty@gmail.com>
2018-03-11 03:03:13 -0700

    added webconfig to backend to correctly serve react router routes

---
+++ src/main/java/com/ucsbmap/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.ucsbmap;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/{spring:\\w+}")
+            .setViewName("forward:/");
+      registry.addViewController("/**/{spring:\\w+}")
+            .setViewName("forward:/");
+      registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+            .setViewName("forward:/");
+  }
+}
